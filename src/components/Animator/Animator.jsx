447de37018a546d7a9a200be3edf0350ccd99454
Brad Larson <brad@bannerwave.com>
2023-10-10 22:29:41 -0700

    added placeholder

--- src/components/Animator/Animator.jsx
+++ src/components/Animator/Animator.jsx
@@ -1,7 +1,17 @@
 import "./Animator.css";
 
 function Animator() {
-  return <>Animator app!!!</>;
+  return (
+    <>
+      <div className="data-frame"></div>
+      <div className="timeline">
+        <div className="seekbar"></div>
+        <div className="rewindbutton"></div>
+        <div className="playbutton"></div>
+        <div className="forwardbutton"></div>
+      </div>
+    </>
+  );
 }
 
 export default Animator;
